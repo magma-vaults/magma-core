0a3ae85c503ce6e055f36d6bb32e201297dbea33
DennisDv24 <DennisDv24@proton.me>
2024-10-31 22:31:25 +0000

    closes #26

--- src/constants.rs
+++ src/constants.rs
@@ -7,7 +7,7 @@ pub const MIN_TICK: i32 = -108_000_000;
 pub const MAX_TICK: i32 = 342_000_000;
 pub const MIN_LIQUIDITY: Uint128 = Uint128::new(1000);
 pub const TWAP_SECONDS: u64 = 60;
-pub static PROTOCOL: Lazy<Addr> = Lazy::new(|| Addr::unchecked("TODO"));
+pub static PROTOCOL: Lazy<Addr> = Lazy::new(|| Addr::unchecked("osmo1a8gd76fw6umx652v7cs73vnge2zju8s8hcm86t"));
 pub static MAX_PROTOCOL_FEE: Lazy<Decimal> = Lazy::new(|| Decimal::from_str("0.1").unwrap());
 
 // NOTE: USDC denom for mainnet.
